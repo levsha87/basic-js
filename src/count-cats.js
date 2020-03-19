0e63f4eb86f7074cdc9ee9a616dcd4439d1e4909
Roma Levsha <rm.levsha@gmail.com>
2020-03-19 23:29:16 +0300

    docs: count Cats - right decision

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,8 +1,12 @@
 module.exports = function countCats(backyard) {
-  let = 0;
- var number = arr.filter(function(number) {
-  if(number["^^"]){
-  number++;}
-});
-  return number;
+  let count = 0;
+  for (let i = 0; i < backyard.length; i++) {
+    backyard[i].forEach(function(a){
+      if(a=="^^"){
+        count++;
+      }
+    });
+  }
+  return count;
 }
+
