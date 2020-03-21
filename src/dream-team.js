27be9386ecebfd14482edae8a9df7d9a7ea10207
Roma Levsha <rm.levsha@gmail.com>
2020-03-21 11:10:56 +0300

    docs: add decision dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let team = [];
+  if(Array.isArray(members)){
+    members.forEach(function (a){
+      if(typeof a === 'string'){
+      let aMembers = a.trim().split('');
+      team.push(aMembers[0].toUpperCase());
+      }
+    });
+  return team.sort().join("");
+  } else {
+    return false;
+  }
+}
